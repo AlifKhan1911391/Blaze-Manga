75ba90cb2549fb960762485b0e878f2328bac54a
Mahadi Islam Alif <iamitachi494@gmail.com>
2026-03-25 15:39:01 +0600

    Create manga-data.js

---
+++ manga-data.js
@@ -0,0 +1,42 @@
+// ============================================
+// MANGA DATA - Edit only this file
+// ============================================
+
+const PAGES_FOLDER = "pages/";
+
+const MANGA = [
+  // Chapter 1
+  {
+    pages: [
+      "pg1.png",
+      "pg2.png",
+      "pg3.png",
+      "pg4.png",
+      "pg5.png",
+      "pg6.png",
+      "pg7.png",
+      "pg8.png",
+      "pg9.png"
+    ]
+  },
+  
+  // Chapter 2
+  {
+    pages: [
+      "pg10.png",
+      "pg11.png",
+      "pg12.png"
+    ]
+  }
+  
+  // ==========================================
+  // ADD NEW CHAPTERS BELOW - Copy this block:
+  // ==========================================
+  // {
+  //   pages: [
+  //     "pg13.png",
+  //     "pg14.png",
+  //     "pg15.png"
+  //   ]
+  // }
+];
